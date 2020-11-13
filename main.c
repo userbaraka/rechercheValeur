a9a52706a66e0ccfc36ea44096367a9f081d5c1e
Moussa Nguette <mossanguette@gmail.com>
2020-11-13 17:19:12 +0000

    imposer l'utilisateur a saisir un reele

--- main.c
+++ main.c
@@ -3,10 +3,21 @@
 void main()
 {  
     float tab[4],a;
-    int i;
+    int i,test;
+    char c;
+            int j=0;
 
-    printf("entrer une valeur\n");
-    scanf("%f",&a);   
+    do
+    {
+        
+        if (j==0)
+            printf("entrer une valeur\n");
+        else
+            printf("vous de devez entrer un reele\n");
+        test=scanf("%f",&a);
+        while ( ((c = getchar()) != '\n') && c != EOF);
+        j++;
+    } while (test!=1);
     int indiceValeurChercher=rechercherValeur(a); //appel de la fonction rechercherValeur
     if (indiceValeurChercher==1)
             printf("la valeur  %.2f est dans le tableau a la position %d\n",tab[indiceValeurChercher],indiceValeurChercher+1);
