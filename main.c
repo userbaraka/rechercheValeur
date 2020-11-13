5b4fc13f701baf120e4bb477a9677017cf69ee9d
becaye2020 <becshala97@gmail.com>
2020-11-13 10:20:54 +0000

    commentaire

--- main.c
+++ main.c
@@ -2,17 +2,17 @@
 #include "main.h"
 void main()
 {  float a;
-    printf("entrer une valeur\n");
-    scanf("%f",&a);
+    printf("entrer une valeur\n");//entrée les valeurs en décimal
+    scanf("%f",&a);// saisir la valeur
     int c=rechercherValeur(a);
     int d=position(a);
     if (c==1)
     {
-        printf("la valeur  %f est dans le tableau a la position %d\n",a,d);
+        printf("la valeur  %f est dans le tableau a la position %d\n",a,d); // décrire la valeur en décimal et la position
     }
     else
     {
-        printf("l element non trouve\n");
+        printf("l element non trouve\n");// la valeur est introuvalble
     }
     
 }
